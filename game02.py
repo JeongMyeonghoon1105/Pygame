a574b20b9307ae73cbc6506417339311d3ace06b
JeongMyeonghoon1105 <wjdaudgns1105@naver.com>
2023-01-09 13:12:31 +0900

    Game 02

--- game02.py
+++ game02.py
@@ -18,14 +18,24 @@ while play:
     # 만약 키보드를 눌렀다면
     if event.type == pygame.KEYDOWN:
       # 누른 키보드에 따라 알맞은 단어 출력
-      if event.key == pygame.K_UP:
+      # if event.key == pygame.K_UP:
+      #   print('UP')
+      # elif event.key == pygame.K_DOWN:
+      #   print('DOWN')
+      # elif event.key == pygame.K_RIGHT:
+      #   print('RIGHT')
+      # elif event.key == pygame.K_LEFT:
+      #   print('LEFT')
+      if event.key == 119:
         print('UP')
-      elif event.key == pygame.K_DOWN:
+      elif event.key == 115:
         print('DOWN')
-      elif event.key == pygame.K_RIGHT:
+      elif event.key == 100:
         print('RIGHT')
-      elif event.key == pygame.K_LEFT:
+      elif event.key == 108:
         print('LEFT')
+    if event.type == pygame.MOUSEBUTTONUP:
+      print('CLICK')
 
 # 게임 종료
 pygame.quit()
