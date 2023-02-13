a0ef6371b9ef85b51b0441dfca9c804e5e00a72b
cobit-git <76054530+cobit-git@users.noreply.github.com>
2023-02-13 15:10:47 +0900

    Completed

--- avoid.py
+++ avoid.py
@@ -33,7 +33,7 @@ e4_x = random.randint(0, 600)
 fps = pygame.time.Clock()
 x = 300
 y = 400
-
+score = 5
 collide = False
 
 # 무한 반복을 통해 게임 실행
@@ -46,67 +46,76 @@ while play:
     # 가져온 이벤트의 타입이 QUIT이면 반복문 종료
     if event.type == pygame.QUIT:
       play = False
-  # 키보드 입력 체크 
-  key = pygame.key.get_pressed()
-  if key[pygame.K_LEFT]:
-    x -= 5
-    if x < 50:
-      x = 50
-  elif key[pygame.K_RIGHT]:
-    x += 5
-    if x > 550:
-      x = 550
-  
-  e1_y += 5
-  e2_y += 5
-  e3_y += 5
-  e4_y += 5
-  if e1_y > 600:
-    e1_x = random.randint(0, 600)
-    e1_y = random.randint(-400, 0)
-  if e2_y > 600:
-    e2_x = random.randint(0, 600)
-    e2_y = random.randint(-400, 0)
-  if e3_y > 600:
-    e3_x = random.randint(0, 600)
-    e3_y = random.randint(-400, 0)
-  if e4_y > 600:
-    e4_x = random.randint(0, 600)
-    e4_y = random.randint(-400, 0)
-  
-  rect1 = sprite.get_rect(center=(x,y))
-  rect2 = e1.get_rect(center=(e1_x,e1_y))
-  rect3 = e1.get_rect(center=(e2_x,e2_y))
-  rect4 = e1.get_rect(center=(e3_x,e3_y))
-  rect5 = e1.get_rect(center=(e4_x,e4_y))
-
-  if rect1.colliderect(rect2):
-    if not collide:
-      print('충돌')
-    collide = True
-  elif rect1.colliderect(rect3):
-    if not collide:
-      print('충돌')
-    collide = True
-  elif rect1.colliderect(rect4):
-    if not collide:
-      print('충돌')
-    collide = True
-  elif rect1.colliderect(rect5):
-    if not collide:
-      print('충돌')
-    collide = True
+  if score <= 0:
+    screen = pygame.image.load('./assets/gameover.png')
+    screen = pygame.transform.scale(screen, (800, 600))
+    background.blit(screen, (-100, 0))
   else:
-    collide = False
+    # 키보드 입력 체크 
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+      x -= 5
+      if x < 50:
+        x = 50
+    elif key[pygame.K_RIGHT]:
+      x += 5
+      if x > 550:
+        x = 550
+    
+    e1_y += 5
+    e2_y += 5
+    e3_y += 5
+    e4_y += 5
+    if e1_y > 600:
+      e1_x = random.randint(0, 600)
+      e1_y = random.randint(-400, 0)
+    if e2_y > 600:
+      e2_x = random.randint(0, 600)
+      e2_y = random.randint(-400, 0)
+    if e3_y > 600:
+      e3_x = random.randint(0, 600)
+      e3_y = random.randint(-400, 0)
+    if e4_y > 600:
+      e4_x = random.randint(0, 600)
+      e4_y = random.randint(-400, 0)
+    
+    rect1 = sprite.get_rect(center=(x,y))
+    rect2 = e1.get_rect(center=(e1_x,e1_y))
+    rect3 = e1.get_rect(center=(e2_x,e2_y))
+    rect4 = e1.get_rect(center=(e3_x,e3_y))
+    rect5 = e1.get_rect(center=(e4_x,e4_y))
 
-  # 배경을 흰색으로 설정
-  background.fill((255, 255, 255))
-  # 스프라이트 복사
-  background.blit(sprite, (x, y))
-  background.blit(e1, (e1_x, e1_y)) 
-  background.blit(e2, (e2_x, e2_y))
-  background.blit(e3, (e3_x, e3_y))
-  background.blit(e4, (e4_x, e4_y))
+    if rect1.colliderect(rect2):
+      if not collide:
+        score -= 1
+        print('점수 : ' + str(score))
+      collide = True
+    elif rect1.colliderect(rect3):
+      if not collide:
+        score -= 1
+        print('점수 : ' + str(score))
+      collide = True
+    elif rect1.colliderect(rect4):
+      if not collide:
+        score -= 1
+        print('점수 : ' + str(score))
+      collide = True
+    elif rect1.colliderect(rect5):
+      if not collide:
+        score -= 1
+        print('점수 : ' + str(score))
+      collide = True
+    else:
+      collide = False
+
+    # 배경을 흰색으로 설정
+    background.fill((255, 255, 255))
+    # 스프라이트 복사
+    background.blit(sprite, (x, y))
+    background.blit(e1, (e1_x, e1_y)) 
+    background.blit(e2, (e2_x, e2_y))
+    background.blit(e3, (e3_x, e3_y))
+    background.blit(e4, (e4_x, e4_y))
   # 디스플레이 업데이트
   pygame.display.update()
 
